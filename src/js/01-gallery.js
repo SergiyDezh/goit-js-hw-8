dd3f60f1695a8d9ad203b228dc0ba184e4d658d3
SergiyDezh <113303472+SergiyDezh@users.noreply.github.com>
2023-03-11 14:17:55 +0200

    goit-js-hw-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+const gallery = document.querySelector(`.gallery`);
+//
+const addGallaryItem = images => {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<div>
+      <a class="gallery__item" href="${original}">
+       <img
+        class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+       />
+      </a>
+     </div>`;
+    })
+    .join(``);
+};
+
+gallery.insertAdjacentHTML(`beforeend`, addGallaryItem(galleryItems));
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const currentTime = 'videoplayer-current-time';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+player.on('timeupdate', throttle(onStart, 1000));
+
+function onStart({ seconds }) {
+  localStorage.setItem(currentTime, seconds);
+}
+player.setCurrentTime(localStorage.getItem(currentTime));
